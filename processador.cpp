c63a28ad64bdeb42b801a51893bdd735781aa996
Wandson Santos <wandsonsantos99@gmail.com>
2018-03-22 21:34:14 -0300

    jmp e goto

--- processador.cpp
+++ processador.cpp
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 
+#define MAX_MEM 16777216
 /*
 UFC - Ciï¿½ncias da Computaï¿½ï¿½o
 Trabalho de Arquitetura de Computadores
@@ -21,8 +21,11 @@ typedef unsigned long int microinstruction;
 
 //Definindo os Registradores;
 word mar,mdr,pc,sp,lv,cpp,tos,opc,h,bA,bB,bC;
-byte mbr,Z,N;
+byte mbr,z,n;
+
 microinstruction firmware[512];
+
+byte ram[MAX_MEM]; // 16 MBytes de RAM
 
 /*Mï¿½todo de leitura dos Registradores de maneira que somente 1 resgistrador
   pode ser lido por vez.*/
@@ -53,7 +56,7 @@ void ler_registrador(byte ender)
 
 void debug()
 {
-	system("clear");
+	system("cls");
 	printf("---Registradores---\n");
 	printf("MAR: %d\n",mar);
 	printf("MDR: %d\n",mdr);
@@ -108,11 +111,11 @@ void alu(byte operacao)
 	}
 
 	if(bC == 0){
-		Z == 1;
-		N == 0;
+		z == 1;
+		n == 0;
 	}else{
-		N == 1;
-		Z == 0;
+		n == 1;
+		z == 0;
 	}
 
 	byte op_desloc = operacao >> 6;
@@ -127,13 +130,20 @@ void alu(byte operacao)
 
 }
 
+word next_address(word next,byte jmp)
+{
+	if(jmp == 0) return next;
+	if(jmp & 1) return next | (z << 8);
+	if(jmp & 2) return next | (n << 8);
+	if(jmp & 4) return next | mbr;
+}
 
 int main()
 {
-	byte mpc = 0,reg_end_lei,op_alu;
+	byte mpc = 0,reg_end_lei,op_alu,jmp;
 	word reg_end_grav,addr;
 	microinstruction mi;
-
+	
 
 	firmware[0] = 0b000000001000001100010000010000000000;
 	firmware[1] = 0b000000010000001101010000010000000100;
@@ -141,25 +151,33 @@ int main()
 	firmware[3] = 0b000000100000001101010000010000000100;
 	firmware[4] = 0b000000000000001101010000010000000100;
 
-
+	op_alu = 0;
 	while(true)
 	{
 		mi = firmware[mpc];
 		reg_end_lei = (mi << 32) >> 32;
 		ler_registrador(reg_end_lei);
 
-
-		op_alu = ( mi<<12 ) >>28;
+		op_alu = (mi << 12) >> 28;
 		alu(op_alu);
+		
 
-		reg_end_grav =(mi << 20) >> 27;
+		reg_end_grav = (mi << 20) >> 27;
 		gravar_registrador(reg_end_grav);
 
 		debug();
-		getchar();
-		
 		addr = mi >> 27;
-		mpc = addr;
+		jmp = ((mi << 9) >> 33);
+		mpc = next_address(addr, jmp);
+				
+		printf("lei %d\n",reg_end_lei);
+		printf("opalu %d\n",alu);
+		printf("addr %d\n",addr);
+		printf("jmp %d\n",jmp);
+		printf("mpc %d\n",mpc);
+		printf("grav %d\n",reg_end_grav);
+	
+		getchar();
 	}
 
 }
